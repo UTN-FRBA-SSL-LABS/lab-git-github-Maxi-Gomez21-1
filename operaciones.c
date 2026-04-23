8567773e65fbb73ca664df73d7982f62b23affb9
Ezequiel Tellez <etellezcori@frba.utn.edu.ar>
2026-04-23 15:51:45 -0300

    Mejora: se agrego un comentario a la funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -2,7 +2,7 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b) {
+int sumar(const int a,const int b) {
     return a + b;
 }
 
@@ -16,7 +16,7 @@ int restar(int a, int b) {
 
 // multiplica dos enteros usando el operador *
 int multiplicar(int a, int b) {
-    return a * b;
+    return a * b; //Se agrega el operador * para realizar la multiplicación
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
